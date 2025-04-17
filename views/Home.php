18dfbd81f0fdc1ec1bde4f3eaefbf164300504aa
Akuras Kurasa <paulandreiscalma@gmail.com>
2025-04-17 11:49:45 +0800

    home, recipes pages complete | recipe page ongoing

---
+++ views/Home.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cooked.</title>
+    <link rel="stylesheet" href="../assets/styles/home.css">
+</head>
+<body>
+        <div class="featuredRecipes">
+            <div class="featuredRecipe chicken">
+                <h1 class="featuredRecipe-name">FOOD</h1>
+            </div>
+
+            <div class="featuredRecipe ramen">
+                <h1 class="featuredRecipe-name">FOOD</h1>
+            </div>
+
+            <div class="featuredRecipe salad">
+                <h1 class="featuredRecipe-name">FOOD</h1>
+            </div>
+
+            <div class="featuredRecipe barbeque">
+                <h1 class="featuredRecipe-name">FOOD</h1>
+            </div>
+        </div>
+
+        <div class="otherRecipes">
+
+            <div class="otherRecipe-wrapper">
+                <div class="otherRecipe pizza"></div>
+                <h3 class="otherRecipe-name">FOOD</h3>
+            </div>
+
+            <div class="otherRecipe-wrapper">
+                <div class="otherRecipe pizza"></div>
+                <h3 class="otherRecipe-name">FOOD</h3>
+            </div>
+
+            <div class="otherRecipe-wrapper">
+                <div class="otherRecipe pizza"></div>
+                <h3 class="otherRecipe-name">FOOD</h3>
+            </div>
+
+            <div class="otherRecipe-wrapper">
+                <div class="otherRecipe pizza"></div>
+                <h3 class="otherRecipe-name">FOOD</h3>
+            </div>
+
+            <div class="otherRecipe-wrapper">
+                <div class="otherRecipe pizza"></div>
+                <h3 class="otherRecipe-name">FOOD</h3>
+            </div>
+
+            <div class="otherRecipe-wrapper">
+                <div class="otherRecipe pizza"></div>
+                <h3 class="otherRecipe-name">FOOD</h3>
+            </div>
+
+            <div class="otherRecipe-wrapper">
+                <div class="otherRecipe pizza"></div>
+                <h3 class="otherRecipe-name">FOOD</h3>
+            </div>
+
+            <div class="otherRecipe-wrapper">
+                <div class="otherRecipe pizza"></div>
+                <h3 class="otherRecipe-name">FOOD</h3>
+            </div>
+
+            <div class="otherRecipe-wrapper">
+                <div class="otherRecipe pizza"></div>
+                <h3 class="otherRecipe-name">FOOD</h3>
+            </div>
+
+        </div>
+</body>
+</html>
---
+++ views/Recipe.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recipe</title>
+    <link rel="stylesheet" href="../assets/styles/recipe.css">
+</head>
+<body>
+    <main>
+        <div class="content">
+            <h1 class="recipeName">Aunt Jemima's Beloved Fried Chicken</h1>
+            <p class="recipeDescription">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non commodi nemo quidem, cumque omnis sit optio aspernatur esse placeat odit praesentium dolorem quo accusamus, provident ad! Ex ullam nemo nihil!
+            </p>
+            <div class="recipeImage"></div>
+            <div class="recipeList">
+                <h1>INGREDIENTS FOR THIS RECIPE</h1>
+
+                <h2>INGREDIENT #1</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores ratione iste consequatur, cupiditate consequuntur animi aliquam fuga ipsum id repudiandae, deleniti eaque facilis debitis molestiae voluptatem vero suscipit quaerat laboriosam.</p>
+            
+                <h2>INGREDIENT #2</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores ratione iste consequatur, cupiditate consequuntur animi aliquam fuga ipsum id repudiandae, deleniti eaque facilis debitis molestiae voluptatem vero suscipit quaerat laboriosam.</p>
+
+                <h2>INGREDIENT #2</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores ratione iste consequatur, cupiditate consequuntur animi aliquam fuga ipsum id repudiandae, deleniti eaque facilis debitis molestiae voluptatem vero suscipit quaerat laboriosam.</p>
+            </div>
+
+            <div class="recipeImage"></div>
+
+            <form action="" id="reviewForm">
+                <h1>Leave A Review</h1>
+
+                <div class="fieldContainer">
+                    <label for="">Subject</label>
+                    <input type="text" name="" id="">
+                </div>
+
+                <div class="fieldContainer">
+                    <label for="">Review</label>
+                    <textarea name="" id=""></textarea>
+                </div>
+
+            </form>
+            
+        </div>
+        <div class="info"></div>
+    </main>
+</body>
+</html>
