198bccc5cc75e7aa8cd129be19db030cbde64d48
Daniel Canlapan <danielcanlapan0402@gmail.com>
2025-05-05 10:36:14 +0800

    upd css

---
+++ components/Footer.php
@@ -0,0 +1,19 @@
+<link rel="stylesheet" href="../assets/styles/footer.css">
+
+<footer>
+    <div class="row">
+        <div class="col">
+            <ul class="nav-logo">
+                <li id="nav-logo-li">
+                    <a href="Home.php">
+                        <h1>Cooked.</h1>
+                    </a>
+                </li>
+            </ul>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe odit a nulla illo itaque et mollitia error, esse quidem voluptatem ut non atque ipsum ad veritatis libero ipsa veniam dicta!</p>
+        </div>
+        <div class="col"></div>
+        <div class="col"></div>
+        <div class="col"></div>
+    </div>
+</footer>
